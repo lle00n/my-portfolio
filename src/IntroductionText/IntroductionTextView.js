e954f3d26e3f7661c578b2d449aa16c5510954d5
Léon Albert <leon.raoul.albert@gmail.com>
2025-03-24 00:31:55 +0100

    WIP

--- src/IntroductionText/IntroductionTextView.js
+++ src/IntroductionText/IntroductionTextView.js
@@ -3,12 +3,15 @@ import { useTranslation } from "react-i18next";
 
 function IntroductionText() {
   const { t } = useTranslation(); 
+  const test = t("introductionText");
   return (
     <div className="introductionTextDiv">
       <div className="introductionTextSection">
         <h1>{t("aboutMeTitle")}</h1>
           <div className="introductionText">
-          <p>{t("introductionText")}</p>
+      {test.split("\n").map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
           </div>
         </div>
           <div className="introductionImage"></div>
