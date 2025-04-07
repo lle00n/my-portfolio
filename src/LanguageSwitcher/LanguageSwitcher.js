762fe7a5d86fa9d3f2d0cdf1e781008a7741b589
Léon Albert <leon.raoul.albert@gmail.com>
2025-04-07 22:52:25 +0200

    Added hiding of language-buttons when scrolling down

--- src/LanguageSwitcher/LanguageSwitcher.js
+++ src/LanguageSwitcher/LanguageSwitcher.js
@@ -6,25 +6,44 @@
  * ----------------------------------------------------------------------------
  */
 
-import React from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import './LanguageSwitcher.css';
 
 const LanguageSwitcher = () => {
   const { t, i18n } = useTranslation();
-
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);
   };
+  const [visible, setVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      console.log("scrollY:", currentScrollY, "last:", lastScrollY);
+
+      if (currentScrollY > lastScrollY) {
+        setVisible(false); // scrolling down
+      } else {
+        setVisible(true); // scrolling up
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
   return (
-    <div className="languageSwitcherDiv">
-      <div class="button-container">
-        <button class="round-button germany" onClick={() => changeLanguage("de")}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/400px-Flag_of_Germany.svg.png" alt="German Flag" class="flag-image" />
+    <div className={`languageSwitcherDiv ${visible ? "visible" : "hidden"}`}>
+      <div className="button-container">
+        <button className="round-button germany" onClick={() => changeLanguage("de")}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/400px-Flag_of_Germany.svg.png" alt="German Flag" className="flag-image" />
         </button>
-        <button class="round-button uk" onClick={() => changeLanguage("en")}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/500px-Flag_of_the_United_Kingdom_%283-5%29.svg.png" alt="UK Flag" class="flag-image" />
+        <button className="round-button uk" onClick={() => changeLanguage("en")}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/500px-Flag_of_the_United_Kingdom_%283-5%29.svg.png" alt="UK Flag" className="flag-image" />
         </button>
       </div>
 
