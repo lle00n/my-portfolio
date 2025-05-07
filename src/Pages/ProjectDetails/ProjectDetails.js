1e3af83eea5cd6ab8a97ea145d0f43001875a0ab
Léon Albert <leon.raoul.albert@gmail.com>
2025-05-07 23:12:24 +0200

    WIP - project-details

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -1,8 +1,23 @@
+/**
+ * ----------------------------------------------------------------------------
+ * Description: Subpage that gives details for the different projects
+ * ----------------------------------------------------------------------------
+ * Author: Léon Albert
+ * ----------------------------------------------------------------------------
+ */
+
+import './ProjectDetails.css';
+import React, { useRef, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
 function ProjectDetails() {
   return (
-    <div>
-      <h1>This is the SubPage</h1>
-      <p>Welcome to the SubPage!</p>
+    <div className="container">
+      <div className="projectDetailsContent">
+        <h1>This is the SubPage</h1>
+        <p>Welcome to the SubPage!</p>
+      </div>
+      <div className="gradientBackground"></div>
     </div>
   );
 }
